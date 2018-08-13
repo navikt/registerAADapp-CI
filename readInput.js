1307656796794950b6f3f637e4445cfa6c64b43a
Kjetil Nordlund <kjetil@jll.no>
2018-08-13 12:51:05 +0200

    circleci

--- readInput.js
+++ readInput.js
@@ -2,7 +2,7 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 
 module.exports = function readInput() {
-    const input = yaml.safeLoad(fs.readFileSync('./applications.yaml', 'utf8'));
+    const input = yaml.safeLoad(fs.readFileSync('~/project/applications.yaml', 'utf8'));
     console.log(input);
     return input;
 };
