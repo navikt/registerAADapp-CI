1cb7cc5ab3341c7f9f41d4c9169f6c6341e36beb
Kjetil Nordlund <kjetil@jll.no>
2018-08-20 12:18:40 +0200

    lagt til konfig for prod

--- app.js
+++ app.js
@@ -2,21 +2,40 @@
 const callAPI = require('./callAPI');
 const readInput = require('./readInput')
 
-const input = readInput();
-// console.log(input)
-
-async function dostuff() {
+// leser preprod konfig og utfører for preprod
+async function doPreprod() {
+    const file = "./applicationsQ.yaml"
+    const input = readInput({ file });
+    // console.log(input)
     for (var i = 0, len = input.Applications.length; i < len; i++) {
         
         const appName = input.Applications[i].name
         const replyURLs = input.Applications[i].replyURLs
         const owners = input.Applications[i].owners
     
-        const result = await callAPI({ appName, replyURLs, owners });
+        const apiURI = process.env.apiURIQ;
+        const result = await callAPI({ apiURI, appName, replyURLs, owners });
         console.log(result);
-
-
     }
 }
 
-dostuff();
+// leser prod konfig og utfører for prod
+async function doProd() {
+    const file = "./applicationsP.yaml"
+    const input = readInput({ file });
+    // console.log(input)
+    for (var i = 0, len = input.Applications.length; i < len; i++) {
+        
+        const appName = input.Applications[i].name
+        const replyURLs = input.Applications[i].replyURLs
+        const owners = input.Applications[i].owners
+    
+        const apiURI = process.env.apiURIP;
+        const result = await callAPI({ apiURI, appName, replyURLs, owners });
+        console.log(result);
+    }
+}
+
+
+doPreprod();
+doProd();
